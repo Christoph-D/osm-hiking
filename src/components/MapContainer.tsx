59231d0513620f83faac2c0563c4da0c2185e7de
Christoph Dittmann <github@yozora.eu>
2025-10-10 23:10:44 +0000

    Prevent panning over the date line

--- src/components/MapContainer.tsx
+++ src/components/MapContainer.tsx
@@ -72,10 +72,17 @@ export function MapContainer() {
       center={initialPosition.center as [number, number]}
       zoom={initialPosition.zoom}
       className="w-full h-full"
+      worldCopyJump={false}
+      maxBounds={[
+        [-90, -180],
+        [90, 180],
+      ]}
+      maxBoundsViscosity={1.0}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        noWrap={true}
       />
       <MapPositionSaver />
       <RouteLayer />
