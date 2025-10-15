90516e9083d8bb3f284897c78e143240bde60ae8
Christoph Dittmann <github@yozora.eu>
2025-10-15 18:51:48 +0000

    cleanup: Remove dead code and unused functions
    Remove unused constants, functions, and test utilities:
    - Remove UNSNAP_FROM_NODE_THRESHOLD constant from waypoints
    - Remove recalculateSegments function from mapHelpers
    - Remove unused test utilities from test/utils.ts
    - Remove createMockRouteWithElevation from test/componentUtils.ts
    - Delete empty test/utils.ts file

--- src/constants/waypoints.ts
+++ src/constants/waypoints.ts
@@ -8,5 +8,4 @@
 export const WAYPOINT_CONSTANTS = {
   CUSTOM_WAYPOINT_THRESHOLD: 100, // meters - create custom if no node within this distance
   SNAP_TO_NODE_THRESHOLD: 50, // meters - auto-snap custom waypoint to node
-  UNSNAP_FROM_NODE_THRESHOLD: 100, // meters - convert node to custom when dragged beyond this
 } as const
